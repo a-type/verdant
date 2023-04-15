6725f660902662015e3ced615ef559a60f094b12
Grant Forrest <gaforres@gmail.com>
2023-04-15 18:44:26 -0400

    retry flaky test

--- test/fileSync.test.ts
+++ test/fileSync.test.ts
@@ -14,53 +14,60 @@ afterAll(() => {
 	fs.rmdirSync('./test-files', { recursive: true });
 });
 
-it('can sync files between replicas', async () => {
-	FileReader.prototype.readAsDataURL = () => {
-		return 'test';
-	};
-	const clientA = await context.createTestClient({
-		library: 'file-sync-1',
-		user: 'User A',
-		logId: 'A',
-	});
-	clientA.sync.start();
+it(
+	'can sync files between replicas',
+	async () => {
+		FileReader.prototype.readAsDataURL = () => {
+			return 'test';
+		};
+		const clientA = await context.createTestClient({
+			library: 'file-sync-1',
+			user: 'User A',
+			logId: 'A',
+		});
+		clientA.sync.start();
 
-	const clientB = await context.createTestClient({
-		library: 'file-sync-1',
-		user: 'User B',
-		logId: 'B',
-	});
-	clientB.sync.start();
+		const clientB = await context.createTestClient({
+			library: 'file-sync-1',
+			user: 'User B',
+			logId: 'B',
+		});
+		clientB.sync.start();
 
-	const a_item = await clientA.items.put({
-		content: 'Apples',
-	});
-	a_item.set('image', createTestFile());
+		const a_item = await clientA.items.put({
+			content: 'Apples',
+		});
+		a_item.set('image', createTestFile());
 
-	await waitForQueryResult(clientB.items.get(a_item.get('id')));
-	console.log('⭐️ item synced to B');
-	const b_item = await clientB.items.get(a_item.get('id')).resolved;
+		await waitForQueryResult(clientB.items.get(a_item.get('id')));
+		console.log('⭐️ item synced to B');
+		const b_item = await clientB.items.get(a_item.get('id')).resolved;
 
-	expect(b_item).toBeTruthy();
-	assert(!!b_item);
-	await waitForCondition(() => !!b_item.get('image'));
-	console.log('⭐️ image synced to B');
-	const file = b_item.get('image')!;
-	await waitForCondition(() => !file.loading);
-	console.log('⭐️ image loaded');
-	expect(file.failed).toBe(false);
-	expect(file.url).toBeTruthy();
+		expect(b_item).toBeTruthy();
+		assert(!!b_item);
+		await waitForCondition(() => !!b_item.get('image'));
+		console.log('⭐️ image synced to B');
+		const file = b_item.get('image')!;
+		await waitForCondition(() => !file.loading);
+		console.log('⭐️ image loaded');
+		expect(file.failed).toBe(false);
+		expect(file.url).toBeTruthy();
 
-	// load the file from the URL and see if it matches.
-	// this isn't the same as the original file, but it's good enough to know
-	// something was delivered...
-	const response = await fetch(file.url!);
-	console.log('⭐️ image fetched');
-	const blob = await response.blob();
-	expect(blob.size).toBe(13);
-	expect(blob.type).toBe('text/plain;charset=utf-8');
-	const text = await blob.text();
-	// basically the file isn't encoded into the form data correctly.
-	// someday maybe I'll get this figured out
-	expect(text).toBe('[object File]');
-}, 20000);
+		// load the file from the URL and see if it matches.
+		// this isn't the same as the original file, but it's good enough to know
+		// something was delivered...
+		const response = await fetch(file.url!);
+		console.log('⭐️ image fetched');
+		const blob = await response.blob();
+		expect(blob.size).toBe(13);
+		expect(blob.type).toBe('text/plain;charset=utf-8');
+		const text = await blob.text();
+		// basically the file isn't encoded into the form data correctly.
+		// someday maybe I'll get this figured out
+		expect(text).toBe('[object File]');
+	},
+	{
+		timeout: 10000,
+		retry: 2,
+	},
+);
