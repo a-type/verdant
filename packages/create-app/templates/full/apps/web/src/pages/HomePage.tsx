dfbe187f69c41415d2123222bc0480ee8f928b79
Grant Forrest <gaforres@gmail.com>
2023-07-07 16:23:45 -0400

    more lo-fis

--- packages/create-app/templates/full/apps/web/src/pages/HomePage.tsx
+++ packages/create-app/templates/full/apps/web/src/pages/HomePage.tsx
@@ -4,20 +4,20 @@ import { TodoList } from '@/components/todos/TodoList.jsx';
 export interface HomePageProps {}
 
 export function HomePage({}: HomePageProps) {
-  return (
-    <div>
-      <h1>Hello lo-fi!</h1>
-      <ul>
-        <li>
-          <Link to="/join">Login or sign up</Link>
-        </li>
-        <li>
-          <Link to="/settings">Settings</Link>
-        </li>
-      </ul>
-      <TodoList />
-    </div>
-  );
+	return (
+		<div>
+			<h1>Hello Verdant!</h1>
+			<ul>
+				<li>
+					<Link to="/join">Login or sign up</Link>
+				</li>
+				<li>
+					<Link to="/settings">Settings</Link>
+				</li>
+			</ul>
+			<TodoList />
+		</div>
+	);
 }
 
 export default HomePage;
--- packages/create-app/templates/local/apps/web/src/pages/HomePage.tsx
+++ packages/create-app/templates/local/apps/web/src/pages/HomePage.tsx
@@ -3,12 +3,12 @@ import { TodoList } from '@/components/todos/TodoList.jsx';
 export interface HomePageProps {}
 
 export function HomePage({}: HomePageProps) {
-  return (
-    <div>
-      <h1>Hello lo-fi!</h1>
-      <TodoList />
-    </div>
-  );
+	return (
+		<div>
+			<h1>Hello Verdant!</h1>
+			<TodoList />
+		</div>
+	);
 }
 
 export default HomePage;
