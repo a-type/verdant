7bfad6fdb51405dc27eaadee81de10237b92f284
Grant Forrest <gaforres@gmail.com>
2023-11-16 09:49:12 -0500

    fix null snapshot migration

--- packages/server/src/migrations/v5.ts
+++ packages/server/src/migrations/v5.ts
@@ -20,7 +20,7 @@ export const procedure = (db: SQL.Database) => {
 		).run(
 			convertLegacyOid(op.oid),
 			// data is already a string, so just leave it and do the conversion on the string
-			replaceLegacyOidsInJsonString(op.data),
+			op.data ? replaceLegacyOidsInJsonString(op.data) : op.data,
 			op.oid,
 			op.timestamp,
 			op.libraryId,
@@ -39,7 +39,9 @@ export const procedure = (db: SQL.Database) => {
     `,
 		).run(
 			convertLegacyOid(baseline.oid),
-			replaceLegacyOidsInJsonString(baseline.snapshot),
+			baseline.snapshot
+				? replaceLegacyOidsInJsonString(baseline.snapshot)
+				: baseline.snapshot,
 			baseline.oid,
 			baseline.libraryId,
 		);
