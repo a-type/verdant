d3462344b21c59bcf88c92ebad9c9a8b35076471
Grant Forrest <grant@butlr.io>
2024-05-01 15:44:15 -0400

    origin transfer tool

--- packages/store/src/backup.ts
+++ packages/store/src/backup.ts
@@ -3,7 +3,10 @@ import Zip from 'jszip';
 import { ExportData } from './metadata/Metadata.js';
 import { ReturnedFileData } from './files/FileStorage.js';
 
-export async function createClientBackup(client: Client) {
+// narrow type to just what's needed
+type BackupClient = Pick<Client, 'export' | 'import'>;
+
+export async function createClientBackup(client: BackupClient) {
 	const exportData = await client.export();
 	const zipFile = new Zip();
 	zipFile.file('data.json', JSON.stringify(exportData.data));
@@ -54,7 +57,119 @@ export async function readBackupFile(file: Blob) {
 	return importData;
 }
 
-export async function importClientBackup(client: Client, file: Blob) {
+export async function importClientBackup(client: BackupClient, file: Blob) {
 	const importData = await readBackupFile(file);
 	await client.import(importData);
+}
+
+/**
+ * Call this function in the app codebase which allows transfer to another origin
+ * to advertise the transfer.
+ */
+export function advertiseTransferTo(client: BackupClient, destination: string) {
+	// listen for 'transfer-accepted' and send transfer
+	window.addEventListener('message', (event) => {
+		if (event.data.type === 'transfer-accepted') {
+			if (event.origin != destination) {
+				console.error(
+					`Received transfer message from unexpected origin: ${event.origin}`,
+				);
+				return;
+			}
+			console.info('Transfer accepted by', event.origin);
+
+			createClientBackup(client).then((file) => {
+				const message = {
+					type: 'transfer',
+					file,
+				};
+				event.source?.postMessage(message, {
+					targetOrigin: event.origin,
+				});
+				console.info('Transfer sent');
+			});
+		}
+	});
+
+	console.log('Advertising transfer to', destination);
+	const message = {
+		type: 'transfer-available',
+	};
+	window.parent.postMessage(message, {
+		targetOrigin: destination,
+	});
+}
+
+/**
+ * Call this function in the app codebase which allows transfer from another origin
+ * to accept the transfer.
+ */
+export function acceptTransferFrom(
+	client: BackupClient,
+	origin: string,
+	onComplete?: () => void,
+) {
+	console.log('Open to accepting transfer from', origin);
+	const frame = document.createElement('iframe');
+	frame.src = `${origin}`;
+	frame.style.display = 'none';
+
+	// listen for 'transfer-available' and begin transfer
+	window.addEventListener('message', async (event) => {
+		if (event.data.type === 'transfer-available') {
+			if (event.origin !== origin) {
+				console.error(
+					`Received transfer message from unexpected origin: ${event.origin}`,
+				);
+				return;
+			}
+			console.log('Transfer available');
+			const message = {
+				type: 'transfer-accepted',
+			};
+			frame.contentWindow?.postMessage(message, origin);
+		} else if (event.data.type === 'transfer') {
+			console.info('Received transfer file');
+			const file = event.data.file;
+			await importClientBackup(client, file);
+			frame.remove();
+			console.info('Transfer complete');
+			onComplete?.();
+		}
+	});
+
+	document.body.appendChild(frame);
+}
+
+export async function transferOrigins(
+	client: BackupClient,
+	from: string,
+	to: string,
+	handlers: {
+		onStart?: () => void;
+		onComplete?: () => void;
+	} = {},
+) {
+	if (window.localStorage.getItem('already-transferred')) {
+		return;
+	}
+
+	function handleComplete() {
+		// remove query param
+		const url = new URL(window.location.href);
+		url.searchParams.delete('transfer');
+		window.history.replaceState({}, '', url.toString());
+		window.localStorage.setItem('already-transferred', 'true');
+		handlers.onComplete?.();
+	}
+
+	if (
+		window.location.origin === to &&
+		new URLSearchParams(window.location.search).get('transfer')
+	) {
+		handlers.onStart?.();
+		acceptTransferFrom(client, from, handleComplete);
+	} else if (window.location.origin === from) {
+		advertiseTransferTo(client, to);
+	}
 }
