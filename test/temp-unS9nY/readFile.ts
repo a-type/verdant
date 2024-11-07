b9c2a74f0eb97357ad766209e2dffb008fc24820
Grant Forrest <gaforres@gmail.com>
2024-11-07 12:58:17 -0500

    remove temp file

--- test/temp-unS9nY/readFile.ts
+++
@@ -1,7 +0,0 @@
-import schema from '../schema.ts';console.log(
-		JSON.stringify(
-			schema,
-			// convert all functions to "FUNCTION"
-			(key, value) => (typeof value === 'function' ? 'FUNCTION' : value),
-		)
-	); process.exit(0);
